337b04bb7a89085aafd65ac108062dfc54af4045
beeresh <beereshp375@gmail.com>
2019-06-01 13:08:13 +0530

    AWS Beanstalk ex2 - in inital commit

---
+++ src/main/java/com/beeresh/aws/AwsElasticBeanstalkEx2Application.java
@@ -0,0 +1,13 @@
+package com.beeresh.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsElasticBeanstalkEx2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsElasticBeanstalkEx2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/beeresh/aws/HelloConroller.java
@@ -0,0 +1,18 @@
+package com.beeresh.aws;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloConroller {
+
+
+
+    @GetMapping
+    public String hello()
+    {
+        return "Hello Youtube";
+    }
+}
